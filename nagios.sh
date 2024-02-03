1086030661a2b40b9d26fcad5f5af1a1bb43c4bd
VARUN KUMAR MANIK <varunmanik1@gmail.com>
2024-02-03 11:21:02 +0800

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -1,70 +1,69 @@
 #!/bin/bash
 
+# Update and install dependencies
 sudo apt update
-sudo apt install -y autoconf bc gawk dc build-essential gcc libc6 make wget unzip apache2 php libapache2-mod-php libgd-dev libmcrypt-dev make libssl-dev snmp libnet-snmp-perl gettext
-sudo mkdir ~/nagios -p
-sudo cd  ~/nagios
-sudo wget https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.6.tar.gz
-sudo tar -xf nagios-4.4.6.tar.gz
-sudo cd nagioscore-nagios-*/
-sudo pwd
+sudo apt install -y autoconf bc gawk dc build-essential gcc libc6 make wget unzip apache2 php libapache2-mod-php libgd-dev libmcrypt-dev libssl-dev snmp libnet-snmp-perl gettext
 
-echo "###2.2	Create the Nagios user and group using the below commands"
+# Create a directory for Nagios installation
+mkdir -p ~/nagios
+cd ~/nagios
+
+# Download and extract Nagios Core
+wget https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.6.tar.gz
+tar -xf nagios-4.4.6.tar.gz
+cd nagioscore-nagios-4.4.6
+
+# Display current directory
+pwd
+
+echo "### 2.2 Creating the Nagios user and group"
 sleep 1
 
+# Create Nagios user and group
 sudo useradd nagios
 sudo groupadd nagcmd
 sudo usermod -a -G nagcmd nagios
 
-
-echo "### 2.3	Compile and install Nagios"
+echo "### 2.3 Compiling and installing Nagios"
 sleep 1
 
+# Configure, compile, and install Nagios
 sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
 sudo make all
 
-### 2.4	Run the following commands to install Nagios binaries, service daemon script, and the command mode.
+# Install Nagios binaries, service daemon script, command mode, and configuration files
 sudo make install
 sudo make install-daemoninit
 sudo make install-commandmode
 sudo make install-config
 
-#### 2.5	Install the Apache configuration for Nagios and activate the mod_rewrite and mode_cgi modules and restart the apache service
+# Install the Apache configuration for Nagios, enable required Apache modules, and restart Apache
 sudo make install-webconf
 sudo a2enmod rewrite cgi
-sudo service apache2 restart
+sudo systemctl restart apache2
 
-#### 2.6	As you have installed the Nagios Core 4.4.6. Now create nagiosadmin user
+# Create a nagiosadmin user with a password
 sudo htpasswd -b -c /usr/local/nagios/etc/htpasswd.users nagiosadmin 123456
 
-#### 2.7	Enter the password of your choice. You need to remember it as this will be used to login to nagios later
-
-#### 2.8	For the firewall configuration, run the command below
-for svc in Apache ssh
-do
-sudo ufw allow $svc
+# Configure firewall
+for svc in Apache ssh; do
+  sudo ufw allow $svc
 done
 
-#### 2.9	To start the UFW firewall service and add it to the system boot, run
+# Enable UFW firewall
 sudo ufw enable
 
-#### Step 3: Installing Nagios and NRPE plugins
+# Installing Nagios and NRPE plugins
+sudo apt install -y monitoring-plugins nagios-nrpe-plugin
 
-#### 3.1	Run the following command:
-
-sudo apt install monitoring-plugins nagios-nrpe-plugin
-
-#### 3.2	Now, go to the nagios installation directory /usr/local/nagios/etc and create a new directory for storing all server hosts configuration
-sudo cd /usr/local/nagios/etc
+# Create a directory for server hosts configurations
 sudo mkdir -p /usr/local/nagios/etc/servers
-#### 3.3	Edit the Nagios configuration nagios.cfg using the vim editor
-#sudo vim nagios.cfg
 
-#### 3.4	Press i and uncomment the cfg_dir option that will be used for storing all server hosts configurations
-sudo echo "cfg_dir=/usr/local/nagios/etc/servers" >> nagios.cfg
-	 
+# Append configuration directory to nagios.cfg
+echo "cfg_dir=/usr/local/nagios/etc/servers" | sudo tee -a /usr/local/nagios/etc/nagios.cfg > /dev/null
 
-#### sudo vim resource.cfg 3.6	Define the Nagios Monitoring Plugins path by changing the default configuration as shown below
-# sudo echo "$USER1$=/usr/lib/nagios/plugins" >> resource.cfg
-# sudo sed -i 's/$USER1$=\/usr/\local\/nagios/\libexec/$USER1$=\/usr\/lib\/nagios\/plugins\/g' resource.cfg
-
+# Note: The following commands for editing resource.cfg and other configurations are commented out
+# as they involve manual editing. Uncomment and modify as necessary.
+# sudo vim /usr/local/nagios/etc/resource.cfg
+# sudo echo "$USER1$=/usr/lib/nagios/plugins" >> /usr/local/nagios/etc/resource.cfg
+# sudo sed -i 's/$USER1$=\/usr\/local\/nagios\/libexec/$USER1$=\/usr\/lib\/nagios\/plugins/g' /usr/local/nagios/etc/resource.cfg
